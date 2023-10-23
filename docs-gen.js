b29b442983c9a13ae04e0356e499e06dd8461254
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-10-23 08:26:10 +0200

    build: updated docs gen script and configuration

--- docs-gen.js
+++ docs-gen.js
@@ -14,16 +14,20 @@ const pnpm = pnpmExec.default;
     await fs.mkdir(tmpDocsGenPath);
 
     for (const remote of docsGenData) {
-        console.log(`Cloning and building ${remote.package}`);
+        console.log(`Cloning and building ${remote.package ?? remote.folder}`);
 
         const branch = "main"; //remote.package && pkgInfo.devDependencies[remote.package] ? `${remote.package}@${pkgInfo.devDependencies[remote.package].replace("^", "")}` : "main";
 
         // TODO: remove comments when the new tsParticles version will be released
         await simpleGit().clone(remote.url, path.join(tmpDocsGenPath, remote.folder), ["--depth", "1", "-b", branch]);
 
+        console.log(`Installing packages on folder ${remote.folder}`);
+
         await pnpm(["install"], {
             cwd: path.resolve(path.join(tmpDocsGenPath, remote.folder)),
         });
+
+        console.log(`Running command ${remote.buildCommand} packages on folder ${remote.folder}`);
 
         await pnpm(["run", remote.buildCommand], {
             cwd: path.resolve(path.join(tmpDocsGenPath, remote.folder)),
@@ -36,25 +40,29 @@ const pnpm = pnpmExec.default;
         //await fs.copy(path.join(tmpDocsGenPath, remote.folder, "docs"), path.join(".", "dist", "docs"));
     }
 
-    const typedocApp = await Application.bootstrapWithPlugins(
-        {
-            options: path.join(docsGenPath, "typedoc.json"),
-            tsconfig: path.join(docsGenPath, "tsconfig.json"),
-        },
-        [new typedoc.TSConfigReader(), new typedoc.TypeDocReader()],
-    );
+    try {
+        const typedocApp = await Application.bootstrapWithPlugins(
+            {
+                options: path.join(docsGenPath, "typedoc.json"),
+                tsconfig: path.join(docsGenPath, "tsconfig.json"),
+            },
+            [new typedoc.TSConfigReader(), new typedoc.TypeDocReader()],
+        );
 
-    const project = typedocApp.convert();
+        const project = typedocApp.convert();
 
-    if (project) {
-        if (!(await fs.pathExists("./dist"))) {
-            await fs.mkdir("./dist");
+        if (project) {
+            if (!(await fs.pathExists("./dist"))) {
+                await fs.mkdir("./dist");
+            }
+
+            // Project may not have converted correctly
+            const outputDir = path.join(".", "dist", "docs");
+            // Rendered docs
+            await typedocApp.generateDocs(project, outputDir);
         }
-
-        // Project may not have converted correctly
-        const outputDir = path.join(".", "dist", "docs");
-        // Rendered docs
-        await typedocApp.generateDocs(project, outputDir);
+    } catch (e) {
+        console.log("Error during docs generation", e);
     }
 
     await fs.remove(tmpDocsGenPath);
